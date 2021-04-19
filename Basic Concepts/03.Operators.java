547d6f14ccb388d163624cf5c5cf1d2afaacd591
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-19 03:01:35 +0300

    Create 03.Operators.java

---
+++ Basic Concepts/03.Operators.java
@@ -0,0 +1,36 @@
+int sum1 = 50 + 10; 
+int sum2 = sum1 + 66; 
+int sum3 = sum2 + sum2;
+
+//
+
+int x = 2; int y = 4;
+
+int result = x + y;
+    System.out.println(result);
+
+//
+
+int x = 15; int y = 4;
+int result = x / y;
+    System.out.println(result);    //output: 3
+
+//You can use double to retrieve a value with a decimal point.
+
+//Modulo
+/**
+The modulo (or remainder) math operation performs an integer division of one value by another, and returns the remainder of that division.
+The operator for the modulo operation is the percentage (%) character.
+*/
+public class Program {
+    public static void main(String[] args) {
+        int value = 23;
+        int res = value % 6;
+
+        System.out.println(res);
+    }
+}
+//Dividing 23 by 6 returns a quotient of 3, with a remainder of 5. Thus, the value of 5 is assigned to the res variable.
+
+
+

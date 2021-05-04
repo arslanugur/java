6e296c75ef200b419b31307830d31c82cccb97c0
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-05 02:34:42 +0300

    Update 02.Variables.java

--- Basic Concepts/02.Variables.java
+++ Basic Concepts/02.Variables.java
@@ -1,9 +1,11 @@
-/**
-Variables have types. Some examples:
-- int: for integers (whole numbers) such as 123 and -456
-- double: for floating-point or real numbers with optional decimal points and fractional parts in fixed or scientific notations, such as 3.1416, -55.66.
-- String: for texts such as "Hello" or "Good Morning!". Text strings are enclosed within double quotes.
-*/
+Variables
+        /*
+        Variables have types. Some examples:
+        - int: for integers (whole numbers) such as 123 and -456
+        - double: for floating-point or real numbers with optional decimal points and fractional parts in fixed or scientific notations, 
+              such as 3.1416, -55.66.
+        - String: for texts such as "Hello" or "Good Morning!". Text strings are enclosed within double quotes.
+        */
 
 String name = "Gosling";
 
@@ -18,32 +20,33 @@ class MyClass {
     }
 }
 
-//Another type is the Boolean type, which has only two possible values: true and false.
+        //Another type is the Boolean type, which has only two possible values: true and false.
 boolean online = true;
 
 
 
-//Getting User Input
-/*
-While Java provides many different methods for getting user input, the Scanner object is the most common, and perhaps the easiest to implement.
-Import the Scanner class to use the Scanner object, as seen here: */
+Getting User Input
+        /*
+        While Java provides many different methods for getting user input, 
+        the Scanner object is the most common, and perhaps the easiest to implement.
+        Import the Scanner class to use the Scanner object, as seen here: */
 import java.util.Scanner;
-//In order to use the Scanner class, create an instance of the class by using the following syntax:
+        //In order to use the Scanner class, create an instance of the class by using the following syntax:
 Scanner myVar = new Scanner(System.in);
 
-/**
-You can now read in different kinds of input data that the user enters.
-Here are some methods that are available through the Scanner class:
-Read a byte - nextByte()
-Read a short - nextShort()
-Read an int - nextInt()
-Read a long - nextLong()
-Read a float - nextFloat()
-Read a double - nextDouble()
-Read a boolean - nextBoolean()
-Read a complete line - nextLine()
-Read a word - next()
-*/
+        /**
+        You can now read in different kinds of input data that the user enters.
+        Here are some methods that are available through the Scanner class:
+        Read a byte - nextByte()
+        Read a short - nextShort()
+        Read an int - nextInt()
+        Read a long - nextLong()
+        Read a float - nextFloat()
+        Read a double - nextDouble()
+        Read a boolean - nextBoolean()
+        Read a complete line - nextLine()
+        Read a word - next()
+        */
 
 import java.util.Scanner;
 
@@ -52,7 +55,5 @@ class MyClass {
         Scanner myVar = new Scanner(System.in);
         System.out.println(myVar.nextLine());        
     }
-}
-
-//input: 2    output: 2
+}           //input: 2    output: 2
 

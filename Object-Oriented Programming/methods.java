571c23bc6c4cb107602f80921d4675e5145ebd42
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-05 22:48:59 +0300

    Create methods.java

---
+++ Object-Oriented Programming/methods.java
@@ -0,0 +1,6 @@
+Methods
+
+
+Methods define behavior. A method is a collection of statements that are grouped together to perform an operation. System.out.println() is an example of a method.
+You can define your own methods to perform your desired tasks.
+Let's consider the following code:

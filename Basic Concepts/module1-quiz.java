9611a74f870b8e3480a22d29c08ec5a5dfc1622f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-08 07:14:01 +0300

    Create module1-quiz.java

---
+++ Basic Concepts/module1-quiz.java
@@ -0,0 +1,11 @@
+      //to declare two variables of type int and print their sum using the sum variable.
+int x = 4;
+int y = 7;
+int sum = x + y;
+
+System.out.println(sum);
+
+      //to output the name
+String name;
+  name = "David";
+    System.out.println(sum);

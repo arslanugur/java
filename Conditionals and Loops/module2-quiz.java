41f956ea05e7249f32a5ca9163939aaccc9666f0
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-08 07:00:59 +0300

    Create module2-quiz.java

---
+++ Conditionals and Loops/module2-quiz.java
@@ -0,0 +1,25 @@
+Module 2 Quiz
+        //to print "in a loop" 7 times, using the while loop.
+int x = 1;
+
+while (x <= 7) {
+  System.out.println("in a loop");
+  x++;
+}
+
+
+        //(a || b ) && c is true if c is true and either a or b is true
+        // a || b is true if either a or b is true
+
+        //if variable "a" is greater than 15, and variable "b" is less than or equal to 72.
+int a = 144;
+int b = 33;
+
+if (a > 15 && b <= 72) {
+  System.out.println("You rock!");
+}
+
+        //to print "in a loop" 5 times using the for loop.
+for (int x = 0; x < 5; x++) {
+  System.out.println("in a loop");
+}

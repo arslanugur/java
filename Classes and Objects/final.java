2626e11be379a1079cf366ad4c3ff446d7cbfcd9
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-12 01:04:18 +0300

    Create final.java

---
+++ Classes and Objects/final.java
@@ -0,0 +1,19 @@
+      //Use the final keyword to mark a variable constant, so that it can be assigned only once.
+
+      //Example:
+class MyClass {
+    public static final double PI = 3.14; 
+    public static void main(String[ ] args) {
+        System.out.println(PI);
+    }
+}    //output: 3.14
+
+      //PI is now a constant. Any attempt to assign it a value will cause an error.
+      
+      //Methods and classes can also be marked final. 
+      //This serves to restrict methods so that they can't be overridden and classes so that they can't be subclassed.
+      //These concepts will be covered in the next module.
+
+      //What keyword makes a variable a constant? --> final
+
+

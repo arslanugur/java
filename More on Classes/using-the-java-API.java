17c23e7bbf72a55361a8b020e646df166a526156
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-21 03:16:29 +0300

    Create using-the-java-API.java

---
+++ More on Classes/using-the-java-API.java
@@ -0,0 +1,19 @@
+Java API
+      The Java API is a collection of classes and interfaces that have been written for you to use.
+      The Java API Documentation with all of the available APIs can be located on the Oracle website at
+      
+      http://docs.oracle.com/javase/7/docs/api/
+
+      Once you locate the package you want to use, you need to import it into your code.
+      The package can be imported using the import keyword.
+
+      For example:
+import java.awt.*;
+
+      The awt package contains all of the classes for creating user interfaces and for painting graphics and images.
+
+      The wildcard character (*) is used to import all of the classes in the package.
+ 
+        
+        
+        

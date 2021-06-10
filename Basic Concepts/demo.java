c1e5a8c0efb6e6628229abcec98f96d8febba881
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-10 05:38:30 +0300

    Update demo.java

--- Basic Concepts/demo.java
+++ Basic Concepts/demo.java
@@ -1,15 +1,16 @@
 # Time Converter
-
-You need a program to convert days to seconds.
-The given code takes the amount of days as input. Complete the code to convert it to seconds and output the result.
-
+	//You need a program to convert days to seconds.
+	//The given code takes the amount of days as input. 
+	//Complete the code to convert it to seconds and output the result.
+/*
 Sample Input:
 12
 Sample Output:
 1036800
   
-Explanation: 12 days are 12*24 = 288 hours, which are 288*60 = 17280 minutes, which are 17280*60 = 1036800 seconds.
-  
+Explanation: 12 days are 12*24 = 288 hours, 
+which are 288*60 = 17280 minutes, which are 17280*60 = 1036800 seconds.
+*/  
 
 import java.util.Scanner;
 

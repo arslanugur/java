85b5c113dd12cd2c3cb277032b1da7e33044a7bf
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-30 21:54:06 +0300

    Create 01.Area of a Circle.java

---
+++ programs/formula/01.Area of a Circle.java
@@ -0,0 +1,26 @@
+class CalculateCircleAreaExample
+{
+
+    public static void main(String[] args)
+    {
+
+        int radius = 3;
+        System.out.println("The radius of the circle is " + radius);
+
+	/*
+         * Area of a circle is pi * r * r where r is a radius of a circle.
+	 */
+
+        // NOTE : use Math.PI constant to get value of pi
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of a circle is " + area);
+    }
+}
+
+/*
+Output:
+The radius of the circle is 3
+Area of a circle is 28.274333882308138
+*/
+

ec17f9931ae3a85878f55dad89c7b73f59926210
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 01:18:08 +0300

    Create 05.Command Line Argument.java

---
+++ programs/basic/05.Command Line Argument.java
@@ -0,0 +1,27 @@
+class CommandLineArgs 
+
+{
+    public static void main(String args[]) 
+    {
+        int a, b, c;
+        
+        a = Integer.parseInt(args[0]);
+        b = Integer.parseInt(args[1]);
+        c = Integer.parseInt(args[2]);
+
+        if (a > b && a > c) {
+            System.out.println("Largest Number is : " + a);
+        } else if (b > c) {
+            System.out.println("Largest Number is : " + b);
+        } else {
+            System.out.println("Largest Number is : " + c);
+        }
+    }
+}
+
+/*
+Output:
+java LargestNumber 2 5 7
+Largest Number is : 7
+*/
+

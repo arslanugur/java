155126c51a4b55349a888878555e40d6fa45f61e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 01:01:19 +0300

    Create 32.String to Boolean Value.java

---
+++ programs/miscellaneous/32.String to Boolean Value.java
@@ -0,0 +1,12 @@
+class MyStringToBoolean
+{
+    public static void main(String a[])
+    {
+        String strBool = "true";
+        Boolean bool = Boolean.parseBoolean(strBool);
+        System.out.println(bool);
+    }
+}
+
+//Output: True
+

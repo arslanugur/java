c4f1f313de0c12f6abbd62a8140e403f8422f397
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 00:53:48 +0300

    Create 26.Date format.java

---
+++ programs/miscellaneous/26.Date format.java
@@ -0,0 +1,39 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.text.DateFormatSymbols;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+class Dates
+{
+    public static void main(final String[] args)
+    {
+        Calendar now = new GregorianCalendar(); //months are 0 indexed, dates are 1 indexed
+        DateFormatSymbols symbols = new DateFormatSymbols(); //names for our months and weekdays
+
+        //plain numbers way
+        System.out.println(now.get(Calendar.YEAR) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE));
+
+        //words way
+        System.out.print(symbols.getWeekdays()[now.get(Calendar.DAY_OF_WEEK)] + ", ");
+        System.out.print(symbols.getMonths()[now.get(Calendar.MONTH)] + " ");
+        System.out.println(now.get(Calendar.DATE) + ", " + now.get(Calendar.YEAR));
+
+        //using DateFormat
+        Date date = new Date();
+        DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(format1.format(date));
+        DateFormat format2 = new SimpleDateFormat("EEEE, MMMM dd, yyyy");
+        System.out.println(format2.format(date));
+    }
+}
+
+/*
+Output:
+2016-3-15
+Tuesday, March 15, 2016
+2016-03-15
+Tuesday, March 15, 2016
+*/
+

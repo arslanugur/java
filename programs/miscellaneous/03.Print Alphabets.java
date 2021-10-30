3f984c5c6fd24de23f4d321fbcfb272d7e754b33
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-30 22:32:36 +0300

    Create 03.Print Alphabets.java

---
+++ programs/miscellaneous/03.Print Alphabets.java
@@ -0,0 +1,26 @@
+class Alphabets
+{
+    public static void main(String args[])
+    {
+        char c = 'A';
+
+        do
+        {
+            System.out.println(c);
+            c++;
+        }
+        while (c <= 'Z');
+    }
+}
+
+/*
+Output:
+A
+B
+.
+.
+.
+Y
+Z
+*/
+

f92ecf3b8d8b5e0c2cc1a83f44babb78893576d6
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 01:03:08 +0300

    Create 34.Math rint Method.java

---
+++ programs/miscellaneous/34.Math rint Method.java
@@ -0,0 +1,17 @@
+class MyRintEx
+{
+    public static void main(String args[])
+    {
+        System.out.println("rint value of 13.345 is: " + Math.rint(13.345));
+        System.out.println("rint value of 38.482 is: " + Math.rint(38.482));
+        System.out.println("rint value of 46.65 is: " + Math.rint(46.65));
+    }
+}
+
+/*
+Output:
+rint value of 13.345 is: 13.0
+rint value of 38.482 is: 38.0
+rint value of 46.65 is: 47.0
+*/
+

fd76620669fc127f091db4601b0f0147afc8a580
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 01:02:19 +0300

    Create 33.Primitive Boolean Constants.java

---
+++ programs/miscellaneous/33.Primitive Boolean Constants.java
@@ -0,0 +1,17 @@
+class MyBooleanConstants
+{
+    public static void main(String a[])
+    {
+        System.out.println("Boolean object corresponding to the primitive value :"
+                                   + Boolean.FALSE);
+        System.out.println("Boolean object corresponding to the primitive value :"
+                                   + Boolean.TRUE);
+    }
+}
+
+/*
+Output:
+Boolean object corresponding to the primitive value :false
+Boolean object corresponding to the primitive value :true
+*/
+

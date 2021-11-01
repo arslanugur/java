cfb7391224e6b782f4e1e9ff582261ebdbc2dceb
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-01 12:37:16 +0300

    Update 03.Variables.kt

--- Kotlin/basic concepts/03.Variables.kt
+++ Kotlin/basic concepts/03.Variables.kt
@@ -32,3 +32,35 @@ fun main(args: Array<String>) {
     num=14*2 
   println(num) 
 } 
+
+
+// to declare a variable called name and assign the value "James" to it.
+var name: String = "Kotlin"
+// var variable_name : Variable type = value:  to declare a variable(a name) which is a string and assign the value "Kotlin" to it.
+
+
+
+// You can also declare variables using the val keyword: 
+fun main(args : Array<String>) {
+    val course: String = "Kotlin"
+    println(course)
+} 
+// The difference with var is that variables declared using val cannot be changed.
+// If a variable must be changeable, then declare it as a var. Otherwise, use val.
+// val is used to declare constant variables, while var is used to declare variables that are not constant. 
+// val = constant and immutable and unchangeable, var != not constant and mutable and changeable
+// Variables declared using the val keyword are immutable. Variables declared using the var keyword are mutable
+
+// to declare a variable named "color" and assign the value 42 to it. The variable should not be changeable.
+val color: Int = 42
+
+// Mostly, there is no need to tell the type of the variable using colon for local variables, 
+// because kotlin does the type inference by itself, and it has a very strong type system.
+
+
+
+
+
+
+
+

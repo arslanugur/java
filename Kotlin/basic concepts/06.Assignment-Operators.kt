9d3db8bcf8a064a8808704c1d27c9825d4587991
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 22:20:49 +0300

    Delete Kotlin directory

--- Kotlin/basic concepts/06.Assignment-Operators.kt
+++
@@ -1,5 +0,0 @@
-// The assignment operator = is used to assign a value to a variable.
-// Kotlin also supports arithmetic assignment operators, for example a+=b is equivalent to a=a+b.
-
-// The same applies to the other operators. For example:
-
